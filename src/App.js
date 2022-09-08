1ff756ca8ed43f9bd0289c5d92919c9065091cca
Feroz Mohammed <feroz.mohammed@mobileprogramming.com>
2022-09-08 11:49:21 +0530

    todo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+import { useState } from "react";
 import "./App.css";
+import Task from "./components/Task";
 
 function App() {
+  const [task, setTask] = useState("");
+  const [taskList, setTaskList] = useState([]);
+  function AddTask() {
+    setTaskList([...taskList, { task: task }]);
+    setTask("");
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <h1> Todo App</h1>
-        <h2>Using React Js</h2>
-      </header>
+      <h1> Todo App</h1>
+      <h2>Using React Js</h2>
+      <input
+        type="text"
+        onChange={(e) => {
+          setTask(e.target.value);
+        }}
+      />
+      <button onClick={AddTask}>Add</button>
+      {/* for showing the information  */}
+      {taskList.map((task) => {
+        return <Task task={task.task} />;
+      })}
     </div>
   );
 }
