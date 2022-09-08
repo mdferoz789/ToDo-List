45551adace38b5959f3c029f2e13b7e4c8b6fa5a
Feroz Mohammed <feroz.mohammed@mobileprogramming.com>
2022-09-08 11:21:26 +0530

    updated commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1> Todo App</h1>
+        <h2>Using React Js</h2>
       </header>
     </div>
   );
